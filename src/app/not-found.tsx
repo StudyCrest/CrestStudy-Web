390c3df00d21e4773eafb2ae2d1a9c149370ef41
Arimoro Olamilekan Ezekiel <67231587+Lexitar32@users.noreply.github.com>
2024-09-27 04:35:36 +0100

    initial project clean up

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,10 @@
 export default function NotFound() {
   return (
-    <div>
-      <h1>Hey, StudyCrest</h1>
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-4">
+        Sorry, we couldn't find the page you're looking for.
+      </p>
     </div>
   );
 }
---
+++ src/context/AppContext.tsx
@@ -0,0 +1,36 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+// Define the type for the context value
+interface AppContextType {
+  state: string;
+  setState: Dispatch<SetStateAction<any>>;
+}
+
+// Initialize the context with undefined as the default value, because null causes the error
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+export function AppProvider({ children }: { children: ReactNode }) {
+  const [state, setState] = useState<string>("");
+
+  return (
+    <AppContext.Provider value={{ state, setState }}>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+// Custom hook to use the context
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  if (context === undefined) {
+    throw new Error("useAppContext must be used within an AppProvider");
+  }
+  return context;
+};
