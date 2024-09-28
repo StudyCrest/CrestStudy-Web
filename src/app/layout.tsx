f6745184105971a47976b35e44cd18fe86982ad6
Arimoro Olamilekan Ezekiel <67231587+Lexitar32@users.noreply.github.com>
2024-09-28 01:30:56 +0100

    initial project set up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,53 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
+import Header from "@components/Header";
+import { Loading } from "@components/Loading";
 import "./globals.css";
+
+export const description =
+  "A solution to help students easily achieve their academic goals by providing tools to organize study schedules, track progress, and maintain motivation";
 
 export const metadata: Metadata = {
   title: "StudyCrest",
-  description: "EdTech App",
+  description,
+  // SEO Metadata
+  keywords: ["Education", "Study", "StudyCrest", "Academics"],
+
+  // Open Graph (OG) tags for social sharing
+  openGraph: {
+    type: "website",
+    url: "https://www.studycrest.io",
+    title: "StudyCrest",
+    description,
+    images: [
+      {
+        url: "/assets/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "StudyCrest Open Graph Image",
+      },
+    ],
+    locale: "en_US",
+  },
+
+  // Twitter Card for sharing on Twitter
+  twitter: {
+    card: "summary_large_image",
+    site: "@your_twitter_handle",
+    title: "StudyCrest",
+    description,
+    images: [
+      {
+        url: "/assets/twitter-image.jpg",
+        alt: "StudyCrest Twitter Card Image",
+      },
+    ],
+  },
+
+  // Canonical URL
+  alternates: {
+    canonical: "https://www.studycrest.io",
+  },
 };
 
 export default function RootLayout({
@@ -13,7 +57,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex flex-col min-h-screen">
+        <Header />
+        <Suspense fallback={<Loading />}>
+          <main className="flex-grow">{children}</main>
+        </Suspense>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { description } from "./layout";
+
+export const metadata = {
+  title: "Home - StudyCrest",
+  description,
+  openGraph: {
+    title: "Home - StudyCrest",
+    description,
+    images: [
+      {
+        url: "/assets/about-og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Home Page Open Graph Image",
+      },
+    ],
+  },
+};
+
 export default function Home() {
   return (
     <div>
-      <h1>Hey, StudyCrest</h1>
+      <h1>Home</h1>
+      <p>This is the Home page.</p>
     </div>
   );
 }
